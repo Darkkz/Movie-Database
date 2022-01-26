54fdf4794180211297b66d34cec87c10d3cf4feb
Darkkz <jake.diedrich@gmail.com>
2022-01-26 00:17:05 -0500

    First Commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res, next) => {
+    res.status(200).sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(port);
+console.log("Server listening at http://localhost:3000");
